7ca40611d02e8fd6c4f1e4067a61f6cd8d7263df
Your Name <your.email@example.com>
2026-03-09 23:17:26 +0530

    chore: sync local changes

--- backend/app/config.py
+++ backend/app/config.py
@@ -17,6 +17,8 @@ class Settings(BaseSettings):
         "https://localhost:3000",
         "http://localhost:3001",
         "https://localhost:3001",
+        "http://localhost:3002",
+        "https://localhost:3002",
     ]
 
     # NLP Model
